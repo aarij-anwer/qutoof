7c2b387e1f4226eaf35a47c10dcfe6bb73db5d85
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2025-09-02 12:02:12 -0400

    update og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: '/og-image.jpg',
-        width: 4267,
-        height: 4267,
+        width: 1200,
+        height: 630,
         alt: 'Qutoof Academy banner',
       },
     ],
