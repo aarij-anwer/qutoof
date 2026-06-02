06490a04f021cc36bb778170fc88d19695396d89
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2026-06-02 04:20:15 -0400

    update config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/arabic-subdomain',
+        has: [{ type: 'host', value: 'arabic.qutoofacademy.com' }],
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/arabic-subdomain/:path*',
+        has: [{ type: 'host', value: 'arabic.qutoofacademy.com' }],
+        destination: '/:path*',
+        permanent: true,
+      },
+    ];
+  },
+
   async rewrites() {
     return [
       {
